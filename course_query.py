53d2ca011ddd964f80ff20530256603e7fbc8ac2
lv1codeman <lv1codeman@gmail.com>
2026-08-05 08:44:32 +0800

    解決學校阻擋海外ip

--- course_query.py
+++ course_query.py
@@ -27,10 +27,12 @@ def download_courses(
 ):
     url = "https://webapt.ncue.edu.tw/deanv2/other/ob010"
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-        "Referer": url,
-        "Origin": "https://webapt.ncue.edu.tw"
-    }
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Language": "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7",
+    "Referer": "https://webapt.ncue.edu.tw/deanv2/other/ob010",
+    "Origin": "https://webapt.ncue.edu.tw"
+}
 
     session = requests.Session()
     try:
